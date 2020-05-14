acdd5a5f2ac732367be51b5c6463be16942af8f5
Lauren Thorud <lthorud12@gmail.com>
2020-05-13 21:49:10 -0500

    debug: url test passed

---
+++ tests/formHandler.test.js
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+
+describe('URL Input Form', () => {
+  it('getUrl retreives the url input from the form', () => {
+    document.body.innerHTML = `
+                <input id="url" type="text" name="url" placeholder="url">
+                <button type='submit' id='submit'>
+        `;
+    require('../src/client/js/formHandler');
+    const { getUrl } = require('../src/client/js/formHandler.js');
+
+    const urlInput = document.getElementById('url');
+    const submitBtn = document.getElementById('submit');
+    urlInput.value = 'https://medium.com';
+
+    const result = getUrl();
+    submitBtn.click();
+
+    expect(result).toBe('https://medium.com');
+  });
+});
