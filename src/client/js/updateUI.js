d650d1e48c1499de681fb2a0056dfadb8d9a569e
Lauren Thorud <lthorud12@gmail.com>
2020-05-14 00:15:15 -0500

    feat: display hashtags and sentiment

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,35 +1,70 @@
-const resultDiv = document.getElementById('results');
+const resultsSection = document.getElementById('results-section');
 
 const processResults = (result) => {
-    let hashtags;
-    let sentiment;
-    let summary;
+  let hashtags;
+  let sentiment;
+  let summary;
 
+  resultsSection.style.height = '100%';
+  resultsSection.style.padding = '2rem';
 
-    for (let i = 0; i < result.length; i++){
-        if (result[i].endpoint === "hashtags") {
-            hashtags = result[i].result.hashtags;
-            console.log('hashtags', hashtags);
-        } else if (result[i].endpoint === "sentiment") {
-            sentiment = result[i].result
-            console.log('sentiment', sentiment);
-        } else if (result[i].endpoint = "summarize") {
-            summary = result[i].result.sentences
-            console.log("summary: ", summary);
-        }
+  for (let i = 0; i < result.length; i++) {
+    if (result[i].endpoint === 'hashtags') {
+      hashtags = result[i].result.hashtags;
+      createHashtags(hashtags);
+    } else if (result[i].endpoint === 'sentiment') {
+      sentiment = result[i].result;
+      console.log('sentiment', sentiment);
+      displaySentiment(sentiment);
+    } else if ((result[i].endpoint = 'summarize')) {
+      summary = result[i].result.sentences;
+      console.log('summary: ', summary);
     }
+  }
 
-    return { hashtags, sentiment, summary };
-}
+  return { hashtags, sentiment, summary };
+};
 
 const updateTitle = (url) => {
-    const title = document.getElementById('results-title')
-    title.textContent = url
-}
+  const title = document.getElementById('results-title');
+  title.textContent = url;
+};
 
 const createHashtags = (hashtags) => {
+  const hashtagList = document.getElementById('hashtag-list');
+  const hashtagTitle = document.getElementById('hashtag-title');
+  hashtagTitle.style.display = 'inline-block';
 
-    
-}
+  while (hashtagList.hasChildNodes()) {
+    hashtagList.removeChild(hashtagList.firstChild);
+  }
 
-export { processResults, updateTitle };
+  for (let i = 0; i < 10; i++) {
+    let li = document.createElement('li');
+    li.textContent = hashtags[i];
+    hashtagList.appendChild(li);
+  }
+};
+
+const displaySentiment = (sentiment) => {
+  const sentimentList = document.getElementById('sentiment-list');
+  const sentimentTitle = document.getElementById('sentiment-title');
+  sentimentTitle.style.display = 'inline-block';
+
+  while (sentimentList.hasChildNodes()) {
+    sentimentList.removeChild(sentimentList.firstChild);
+  }
+
+  const polarity = sentiment.polarity;
+  const confidence = sentiment.polarity_confidence.toFixed(2);
+  const percentage = confidence * 100;
+
+  const cLi = document.createElement('li');
+  const pLi = document.createElement('li');
+  pLi.textContent = 'Sentiment: ' + polarity;
+  cLi.textContent = 'Confidence: ' + percentage + '%';
+  sentimentList.appendChild(cLi);
+  sentimentList.appendChild(pLi);
+};
+
+export { processResults, updateTitle, createHashtags, displaySentiment };
