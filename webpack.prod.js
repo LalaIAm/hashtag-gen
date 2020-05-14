d46a12425df07e1d09d19b5f9fcdb267933d16db
Lauren Thorud <lthorud12@gmail.com>
2020-05-14 04:45:50 -0500

    l

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   entry: {
-    main: './src/client/index.js',
+    main: './src/client/index.js'
   },
   mode: 'production',
   output: {
